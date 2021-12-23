c61cc1616643ab7c7202b3e17945ece0e4fa3a9e
lvfd <lvfd@acca.com.cn>
2021-12-23 17:32:59 +0800

    修改登录流程对接德付通，sm4key未定义

--- express.js/index.js
+++ express.js/index.js
@@ -31,6 +31,9 @@ dovepay_freight.set('views', path.join(root_path, 'views'))
 dovepay_freight.engine(views_ext, ejs.__express)
 dovepay_freight.use(express.static(path.join(root_path, 'public')))
 
+// Urls:
+const { urls } = require('./config')
+
 // Use Helmet:
 dovepay_freight.use(helmet({
   contentSecurityPolicy: false
@@ -52,8 +55,69 @@ if (dovepay_freight.get('env') === 'production') {
 dovepay_freight.use(session(sess))
 
 // Routers: 
+// Test code:
 dovepay_freight.get('/', (req, res) => {
-  res.render('demoindex')
+  let params = {
+    userId: '516171584@qq.com',
+    urls: urls,
+    pagefor: 'user',
+    title: '用户参数'
+  }
+  res.render('demoindex', params)
+})
+dovepay_freight.get('/mgr', (req, res) => {
+  let params = {
+    userId: '516171584@qq.com',
+    urls: urls,
+    pagefor: 'system',
+    title: '系统商参数'
+  }
+  res.render('demoindex', params)
+})
+
+// Link dovePay:
+const Jssm4 = require('jssm4')
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+dovepay_freight.post('/', (req, res) => {
+  const key = 'FA171555405706F73D7B973DB89F0B47'
+  const sm4 = new Jssm4(key)
+  let userId = ''
+  try {
+    const userId_raw = req.body.userId
+    userId = sm4.decryptData_ECB(userId_raw)
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
+  if (!userId) return;
+  const params = {
+    userId: userId,
+    urls: urls,
+    pagefor: 'user',
+    title: '用户参数'
+  }
+  res.render('demoindex', params)
+})
+dovepay_freight.post('/mgr', (req, res) => {
+  const key = 'FA171555405706F73D7B973DB89F0B47'
+  const sm4 = new Jssm4(key)
+  let userId = ''
+  try {
+    const userId_raw = req.body.userId
+    userId = sm4.decryptData_ECB(userId_raw)
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
+  if (!userId) return;
+  const params = {
+    userId: userId,
+    urls: urls,
+    pagefor: 'system',
+    title: '系统商参数'
+  }
+  res.render('demoindex', params)
 })
 dovepay_freight.use('/log', rtlogin)
 dovepay_freight.use('/agent', rtagent)
