d53894888ab14dc0c9eb9e264417662210dd10a0
lvfd <lvfd@acca.com.cn>
2021-12-28 12:59:20 +0800

    添加测试库sm4key

--- express.js/config/index.js
+++ express.js/config/index.js
@@ -1,5 +1,15 @@
 const urls = require('./urls')
+const sm4keys = {
+  test: {
+    freight_user: 'Fa3HGo1AW2GN8zWS',
+    freight_system: 'Fa3HGo1AW2GN8zWS'
+  },
+  prod: {
+    freight_user: '',
+    freight_system: ''
+  }
+}
 
 module.exports = {
-  urls
+  urls, sm4keys
 }
