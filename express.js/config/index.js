6356a7bb2da450caa458a7725a86bd02d68bfb40
lvfd <lvfd@acca.com.cn>
2022-01-12 09:49:26 +0800

    加入redis集群配置、cookie名称

--- express.js/config/index.js
+++ express.js/config/index.js
@@ -9,7 +9,21 @@ const sm4keys = {
     freight_system: ''
   }
 }
+const redisConfig = {
+  rootNodes: [
+    { host: '10.1.85.160', port: 7000 },
+    { host: '10.1.85.160', port: 7001 },
+    { host: '10.1.85.160', port: 7002 },
+    { host: '10.1.85.160', port: 7003 },
+    { host: '10.1.85.160', port: 7004 },
+    { host: '10.1.85.160', port: 7005 }
+  ],
+  password: 'Acca@1234'
+}
+const cookieConfig = {
+  name: 'dovepay.connect.sid'
+}
 
 module.exports = {
-  urls, sm4keys
+  urls, sm4keys, redisConfig, cookieConfig
 }
