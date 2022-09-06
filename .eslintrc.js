c94ef64bb8afb62197a4c1f16fa8089d3f2cefdf
lvfd <lvfd@acca.com.cn>
2022-09-06 09:43:55 +0800

    Set eslint, dist file, etc.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        "jquery": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:json/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "json"
+    ],
+    "rules": {
+    },
+    "globals": {
+        "UIkit": "readonly"
+    }
+}
