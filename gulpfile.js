9a2bf6359f5c3a7d695024372fe629a243adcad2
lvfd <lvfd@acca.com.cn>
2023-03-09 10:40:46 +0800

    fix Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest } = require('gulp')
 const path = require('path')
 
 exports.default = function() {
-  return src(path.resolve(__dirname, '../', 'dovepay-freight-ui/release/dovepayFreight.min.js'))
+  // return src(path.resolve(__dirname, '../', 'dovepay-freight-ui/release/dovepayFreight.min.js'))
+  return src(require('@lyufudi/dovepay-freight-ui/release/dovepayFreight.min.js'))
     .pipe(dest('dist/'))
 }
